d912398cf081b1325625535179e4cb5b02cd1ca9
Jeff Mignone <jeff.mignone@gmail.com>
2018-06-02 17:54:37 -0400

    setting up server.js for testing

--- server.js
+++ server.js
@@ -11,6 +11,37 @@ app.use(morgan('common'));
 
 app.use('/blog-posts', blogPostsRouter);
 
-app.listen(PORT, () => {
-    console.log(`Your app is listening on port ${PORT}`);
-});
+let server;
+
+function runServer() {
+    const port = process.env.PORT || 8080;
+    return new Promise((resolve, reject) => {
+        server = app.listen(port, () => {
+            console.log(`Your app is listening on port ${port}`);
+            resolve(server);
+        }).on('error', err => {
+            reject(err);
+        });
+    });
+}
+
+
+function closeServer() {
+    return new Promise((resolve, reject) => {
+        console.log('closing server');
+        server.close(err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            resolve();
+        });
+    });
+}
+
+if (require.main === module) {
+    runServer().catch(err => console.log(err));
+}
+
+module.exports = { app, runServer, closeServer };
+
